0a9d58c5df3a9ec8208e93ce2da2233b5058e5fd
DulceAhtziry <96158094+DulceAhtziry@users.noreply.github.com>
2023-04-25 11:51:46 -0600

    EjercicioDados

---
+++ juegos_dados/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'juegos_dados';
+
+  public valor1 : number = 0;
+  public valor2 : number = 0;
+  public valor3 : number = 0;
+  public resultado : string = "";
+
+  public numeroAleatorio() : number {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  constructor(){
+    this.valor1 = this.numeroAleatorio();
+    this.valor2 = this.numeroAleatorio();
+    this.valor3 = this.numeroAleatorio();
+  }
+
+  public tirar() : void{
+    this.valor1 = this.numeroAleatorio();
+    this.valor2 = this.numeroAleatorio();
+    this.valor3 = this.numeroAleatorio();
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+      this.resultado = 'Ganó C: ';
+    }else{
+      this.resultado= 'Perdio :C ';
+    }
+  }
+}
+
